0883787b9c81b6d1089dfb77449f98c252526a81
webberic92 <36771546+webberic92@users.noreply.github.com>
2019-07-31 02:00:35 -0400

    Add files via upload

---
+++ stringInputKeyValuePairs/src/main/playground.java
@@ -0,0 +1,22 @@
+package main;
+import java.util.Scanner;
+
+public class playground {
+
+	public static void main(String[] args) {
+		System.out.println("Please enter your key value pair in a string..");
+		
+		//Scanner select = new Scanner(System.in);
+		//String inputString = select.nextLine();
+		
+		String inputString = "firstName=pijush&lastName=santra&confirmationNumber=1207125059";
+		System.out.println("Your string was...");
+		System.out.println(inputString);
+		}
+
+			
+		}
+		
+		
+		
+	
